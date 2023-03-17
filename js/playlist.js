9ee8ef1277723911e7b35b6716ad331cb18634bd
Romain Valls <romain.valls95@gmail.com>
2023-03-17 10:09:27 +0100

    first commit

---
+++ js/playlist.js
@@ -0,0 +1,26 @@
+let songs = [
+    {
+        name : "Riri - Othello",
+        artist : "Othello",
+        img : "img1",
+        audio : "music1",
+    },
+    {
+        name : "Pause - Othello",
+        artist : "Othello",
+        img : "img2",
+        audio : "music2",
+    },
+    {
+        name : "Noizay - Othello",
+        artist : "Othello",
+        img : "img3",
+        audio : "music3",
+    },
+    {
+        name : "Jesmo - Othello",
+        artist : "Othello",
+        img : "img4",
+        audio : "music4",
+    },
+]
